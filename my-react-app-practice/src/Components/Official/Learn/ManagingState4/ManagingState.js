739591216997931b367d4e2edb22c6c0780792d4
sonukryadav <sonukryadav56@gmail.com>
2022-12-28 01:15:28 +0530

    Sharing state b/w compo...

--- my-react-app-practice/src/Components/Official/Learn/ManagingState4/ManagingState.js
+++ my-react-app-practice/src/Components/Official/Learn/ManagingState4/ManagingState.js
@@ -4,12 +4,16 @@ import ChoosingTheStateStructure2 from './ChoosingTheStateStructure2';
 
 const ManagingState = () => {
     return (
-        <div>
+        <div style={{backgroundColor:'lightgray'}}>
             <hr />
             <hr />
-            <h1>Managing State</h1>
+            <h1>-: Managing State4 :-</h1>
+            <h2 style={{color:'blue',textDecoration:'underline'}}>Reacting to input with state</h2>
             <Form />
-            <ChoosingTheStateStructure2 />
+            <h2 style={{ color: 'blue', textDecoration: 'underline'}}>Sharing state between components</h2>
+            <Accordion />
+            <h2 style={{ color: 'blue', textDecoration: 'underline' }}>Extracting state logic into a reducer</h2>
+            {/* <ChoosingTheStateStructure2 /> */}
         </div>
     );
 }
@@ -71,6 +75,36 @@ const submitForm = (answer) => {
     });
 }
 // ----------xxx-----------------City quiz------------xxx-------------------------//
+
+
+//-----------------Sharing state between components------------------------
+
+function Accordion() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    return (
+        <>
+            <h2>Almaty, Kazakhstan</h2>
+            <Panel title="About" isActive={activeIndex === 0} onShow={()=> setActiveIndex(0)} >
+                With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+            </Panel>
+            <Panel title="Etymology" isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}>
+                The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+            </Panel>
+        </>
+    );
+}
+
+
+const Panel = ({ title, children, isActive, onShow}) => {
+    return (
+        <section>
+            <h3>{title}</h3>
+            {isActive ? (<p>{ children }</p>) :(<button onClick={onShow}>Show</button>)}
+        </section>
+    );
+}
+
+//--------xxxx---Sharing state between components----------xxx-------------
 
 export default ManagingState;
 
