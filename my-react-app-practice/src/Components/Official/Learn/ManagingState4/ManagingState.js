67b0036cfc5f8a5d9e2ad9ef8485cd88549e0fab
sonukryadav <sonukryadav56@gmail.com>
2022-12-28 11:40:12 +0530

    done todo app

--- my-react-app-practice/src/Components/Official/Learn/ManagingState4/ManagingState.js
+++ my-react-app-practice/src/Components/Official/Learn/ManagingState4/ManagingState.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import ChoosingTheStateStructure2 from './ChoosingTheStateStructure2';
 
 
@@ -13,6 +13,7 @@ const ManagingState = () => {
             <h2 style={{ color: 'blue', textDecoration: 'underline'}}>Sharing state between components</h2>
             <Accordion />
             <h2 style={{ color: 'blue', textDecoration: 'underline' }}>Extracting state logic into a reducer</h2>
+            <TaskApp />
             {/* <ChoosingTheStateStructure2 /> */}
         </div>
     );
@@ -103,8 +104,166 @@ const Panel = ({ title, children, isActive, onShow}) => {
         </section>
     );
 }
-
 //--------xxxx---Sharing state between components----------xxx-------------
+
+
+//----------Extracting state logic into a reducer-------------------
+const initialTasks = [
+    { id: 0, text: 'Visit Kafka Museum', done: true },
+    { id: 1, text: 'Watch a puppet show', done: false },
+    { id: 2, text: 'Lennon Wall pic', done: false }
+];
+let nextId = 3;
+
+function TaskApp() {
+    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+    function handleAddTask(text) {
+        dispatch({
+            type: 'added',
+            id: nextId++,
+            text: text,
+        });
+    }
+
+    function handleChangeTask(task) {
+        dispatch({
+            type: 'changed',
+            task: task
+        });
+    }
+
+    function handleDeleteTask(taskId) {
+        dispatch({
+            type: 'deleted',
+            id: taskId
+        });
+    }
+
+    return (
+        <>
+            <h3>Prague itinerary</h3>
+            <AddTask onAddTask={handleAddTask} />
+            <TaskList tasks={tasks}
+                onChangeTask={handleChangeTask}
+                onDeleteTask={handleDeleteTask}
+            />
+        </>
+    );
+}
+
+function tasksReducer(tasks, action) {
+    switch (action.type) {
+        case 'added': {
+            return [...tasks, {
+                id: action.id,
+                text: action.text,
+                done: false
+            }];
+        }
+        case 'changed': {
+            return tasks.map(t => {
+                if (t.id === action.task.id) {
+                    return action.task;
+                } else {
+                    return t;
+                }
+            });
+        }
+        case 'deleted': {
+            return tasks.filter(t => t.id !== action.id);
+        }
+        default: {
+            throw Error('Unknown action: ' + action.type);
+        }
+    }
+}
+
+const AddTask = ({ onAddTask }) => {
+    const [text, setText] = useState('');
+    return (
+        <>
+            <input type="text" placeholder='Add task' value={text} onChange={e => setText(e.target.value)} />
+            <button onClick={() => {
+                setText('');
+                onAddTask(text);
+            }}>Add</button>
+        </>
+    );
+}
+
+const TaskList = ({ tasks, onChangeTask, onDeleteTask}) => {
+    return (
+        <>
+            <ul>
+                {tasks.map(task =>{
+                    return <li key={task.id}>
+                        <Task
+                            task={task}
+                            onChange={onChangeTask}
+                            onDelete={onDeleteTask}
+                        />
+                    </li>
+                })}
+            </ul>
+        </>
+    );
+}
+
+function Task({ task, onChange, onDelete }) {
+    const [isEditing, setIsEditing] = useState(false);
+    let taskContent;
+    if (isEditing) {
+        taskContent = (
+            <>
+                <input
+                    value={task.text}
+                    onChange={e => {
+                        onChange({
+                            ...task,
+                            text: e.target.value
+                        });
+                    }} />
+                <button onClick={() => setIsEditing(false)}>
+                    Save
+                </button>
+            </>
+        );
+    } else {
+        taskContent = (
+            <>
+                {task.text}
+                <button onClick={() => setIsEditing(true)}>
+                    Edit
+                </button>
+            </>
+        );
+    }
+    return (
+        <label>
+            <input
+                type="checkbox"
+                checked={task.done}
+                onChange={e => {
+                    onChange({
+                        ...task,
+                        done: e.target.checked
+                    });
+                }}
+            />
+            {taskContent}
+            <button onClick={() => onDelete(task.id)}>
+                Delete
+            </button>
+        </label>
+    );
+}
+
+
+
+
+
+//-----xxx---Extracting state logic into a reducer----xxx-----------
 
 export default ManagingState;
 
